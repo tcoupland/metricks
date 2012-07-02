0ae234b614029bbba56cbba0dc319a4d152aeef9
Tom Coupland <tcoupland@gmail.com>
2012-07-02 21:59:45 +0100

    lein-release plugin: bumped version from 0.1.6 to 0.1.7-SNAPSHOT for next developemnt cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mantree/metricks "0.1.6"
+(defproject org.clojars.mantree/metricks "0.1.7-SNAPSHOT"
   :description "Wrapper for yammer metrics project that creates metrics based on function meta data."
   :url "https://github.com/mantree/metricks"
   :license {:name "Eclipse Public License"
