7ba5883308b6afb636dd0b99037fae226cca4a8a
Tom Coupland <tom.coupland@nokia.com>
2013-09-30 11:37:51 +0100

    Incrementing version

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject org.clojars.mantree/metricks "0.1.9-SNAPSHOT"
+(defproject org.clojars.mantree/metricks "0.1.10-SNAPSHOT"
   :description "Wrapper for yammer metrics project that creates metrics based on function meta data."
   :url "https://github.com/mantree/metricks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.yammer.metrics/metrics-core "2.1.2"]]
-  :lein-release {:deploy-via :clojars})
+                 [com.yammer.metrics/metrics-core "2.1.2"]])
