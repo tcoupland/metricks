0e9764b13434bf10c7dec96743a0ddc0c88b9491
Tom Coupland <tom.coupland@nokia.com>
2013-09-30 11:38:02 +0100

    Increasing to clojure 1.5

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/mantree/metricks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [com.yammer.metrics/metrics-core "2.1.2"]])
